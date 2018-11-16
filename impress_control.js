b1af9c81b6c26ea9d84919b6dafe6e14ad83efab
guruyaya <guruyaya@gmail.com>
2018-11-16 15:34:30 +0200

    Simple way to get to any poem

--- impress_control.js
+++ impress_control.js
@@ -1,4 +1,32 @@
 (function(){
+    var all_pages = document.getElementById('all_pages');
+    var slides = document.querySelectorAll('.slide');
+    for(var i=0;i<slides.length;i++){
+        var slide = slides[i];
+        var option = document.createElement('option');
+        option.value = slide.id;
+        if (slide.classList.contains('section'))
+            option.innerHTML = "* * " + slide.id + " * *";
+        else
+            option.innerHTML = slide.id;
+        all_pages.appendChild(option);
+    }
+    all_pages.addEventListener('change', function(){
+        impress().goto(all_pages.options[all_pages.selectedIndex].value);
+    });
+    var current_location = (decodeURIComponent(window.location.hash).substr(1));
+    impress().init();
+    setTimeout(function(){
+        impress().goto(current_location);
+    }, 100);
+
+    var rootElement = document.getElementById( "impress" );
+    rootElement.addEventListener( "impress:stepenter", function(event) {
+      var currentStep = event.target;
+      var targetOption = document.querySelector('#all_pages option[value='+currentStep.id+']');
+      targetOption.selected = 'selected';
+    });
+
     window.HorizontalMove = function(factor){
         var impress_root = document.querySelector('#impress>div');
         var transformVars = impress_root.style.transform;
